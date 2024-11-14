0596944f02c359b224ee546400e37299d747268c
da-alb <76657647+da-alb@users.noreply.github.com>
2024-11-14 23:05:09 +0100

    Create a insert room data function.

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import sys
-from credentials import cur
+import mariadb
+from credentials import cur, conn
 
-cur.execute("SELECT * from orders;")
+ins_room_name = input("Please insert the room name: ")
+
+def insert_room(room_name):
+    try:
+        sql_query=("insert into rooms (name) values (%s);")
+        sql_data = (room_name,)
+        cur.execute(sql_query, sql_data)
+        conn.commit()
+    except mariadb.Error as e:
+        print(f"Error adding entry to database: {e}")
+    finally:
+        if conn:
+            conn.close()
+
+insert_room(ins_room_name)
